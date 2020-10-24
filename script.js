880960c0b5369dcf08cbd13c20518f5030c14035
pepe30 <jose.pinedaesquer@gmail.com>
2020-10-24 15:15:57 -0500

    changed code for go back button click event to refresh page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //initialize questions, options and answers
 
-var question = ["HTML stands for?","which of the following tag is used to mark a begining of paragraph","Correct HTML tag for the largest heading is","Markup tags tell the web browser","What are Empty elements and is it valid?","Which of the following attributes of text box control allow to limit the maximum character?","Web pages starts with which ofthe following tag?","The attribute which define the relationship between current document and HREF'ed URL is","Which of the following is a container?","The tag used to create a new list item and also include a hyperlink is","Can the element <First> be replaced with <first>","The tag used to create a hypertext relationship between current document and another URL is","Which built-in method calls a function for each element in the array?","Which of the following function of String object returns the index within the calling String object of the first occurrence of the specified value?","Which of the following function of String object returns the calling string value converted to lower case while respecting the current locale?","Which of the following function of Array object returns a new array comprised of this array joined with other array(s) and/or value(s)?","Which of the following function of Array object returns a string representing the array and its elements?","Which of the following is a valid type of function javascript supports?"];
+var questionini = ["HTML stands for?","which of the following tag is used to mark a begining of paragraph","Correct HTML tag for the largest heading is","Markup tags tell the web browser","What are Empty elements and is it valid?","Which of the following attributes of text box control allow to limit the maximum character?","Web pages starts with which ofthe following tag?","The attribute which define the relationship between current document and HREF'ed URL is","Which of the following is a container?","The tag used to create a new list item and also include a hyperlink is","Can the element <First> be replaced with <first>","The tag used to create a hypertext relationship between current document and another URL is","Which built-in method calls a function for each element in the array?","Which of the following function of String object returns the index within the calling String object of the first occurrence of the specified value?","Which of the following function of String object returns the calling string value converted to lower case while respecting the current locale?","Which of the following function of Array object returns a new array comprised of this array joined with other array(s) and/or value(s)?","Which of the following function of Array object returns a string representing the array and its elements?","Which of the following is a valid type of function javascript supports?"];
 
 var game = [
 
@@ -24,11 +24,13 @@ var game = [
     ['A. named function','B.  anonymous function','C.  Both of the above. ','D.  None of the above.']
 
 ];
-var solution = ["A","C","D","B","B","C","C","A","D","A","B","C","C","D","A","A","D","C"];
+var solutionini = ["A","C","D","B","B","C","C","A","D","A","B","C","C","D","A","A","D","C"];
 
 //set variables
 
 var options = [];
+var solution = [];
+var question = [];
 var totalq = question.length;
 var questiontext = document.querySelector("#Question");
 var start = document.querySelector("#start");
@@ -51,133 +53,130 @@ var timeron = 0;
 //create functions
 
     //set time interval
+    function startTime(){
+        
+        
+        timeron = setInterval(function(){
+        timer.textContent = "Time : " + timeplay;
+        timeplay--;
+        if(timeplay<1)
+        {endGame();}
+        },1000);
 
-
-
-function startTime(){
     
-    
-    timeron = setInterval(function(){
-    timer.textContent = "Time : " + timeplay;
-    timeplay--;
-    if(timeplay<1)
-    {endGame();}
-    },1000);
-
-   
-}
-
-
-function stopTime(){
-    clearInterval(timeron);
-}
-
-function startGame()
-{   
-    options = game;
-    timeplay = 60;
-    startTime();
-    
-    //document.getElementById("score").style.display = "none";
-    instructions.setAttribute("style","display: none");
-    questioncard.setAttribute("style","display: initial");
-    setquestion();
-    score = 0;
-    gametime = 0;
-    gameon = 1;
-    
-}
-
-function endGame()
-{
-    timeplay = 0;
-    timer.textContent = "";
-    stopTime();
-    result.textContent = "Time Out!";
-    setTimeout(function(){
-
-        gametime = timeplay;
-    
-        document.getElementById("card").style.display = "none";
-        document.getElementById("score").style.display = "initial";
-        document.getElementById("finalscore").textContent = "Your final Score is "+ score+ " and your playtime is "+ gametime + " seconds";
-
     }
-        ,1000);
-    
-    
-   
 
-}
+    function stopTime(){
+            clearInterval(timeron);
+    }    
 
-function resetGame(){
-    scorecard.setAttribute("style","display: none");
-    document.getElementById("instructions").style.display = "initial";
-
-}
-
-function setquestion()
-{ 
-    
-    if(question.length > 0 && timeplay > 0)
-    {
-        result.textContent = "";
-        document.getElementById("choices").innerHTML = "";
-        document.getElementById("choices").style.pointerEvents = "initial";
-
-        //choose a random question
-        current = Math.floor(Math.random() * question.length);
-        questiontext.textContent = question[current];
+    function startGame()
+    {   
+        inializeQuestions();
+        timeplay = 60;
+        startTime();
         
-        //set choices
-        for(var i =0;i<4;i++)
-        {   var setchoices = document.createElement("li");
-            var newbutton = document.createElement("button");
-            newbutton.classList.add("btn-info");
-            newbutton.classList.add("rounded-pill");
-            newbutton.classList.add("m-1");
-            newbutton.textContent = options[current][i];
-            setchoices.appendChild(newbutton);
-            choices.appendChild(setchoices);
-        }
+        //document.getElementById("score").style.display = "none";
+        instructions.setAttribute("style","display: none");
+        questioncard.setAttribute("style","display: initial");
+        setquestion();
+        score = 0;
+        gametime = 0;
+        gameon = 1;
         
-        //set answer
-        answer = solution[current];
-        
-        //Remove answer from list
-         question.splice(current,1);
-        options.splice(current,1);
-        solution.splice(current,1);
-        console.log(question);
-       
-        
-
-
     }
-    else
+
+    function endGame()
     {
-        timeplay = 0;
-        result.classList.remove();
-        result.classList.add("border-top");
-        result.classList.add("text-secondary");
-        result.textContent = "Game over";
-        gametime = timeplay;
+        
+        timer.textContent = "";
         stopTime();
-        document.getElementById("card").style.display = "none";
-        document.getElementById("score").style.display = "initial";
-        document.getElementById("finalscore").textContent = "Your final Score is "+ score+ " and your playtime is "+ gametime + " seconds";
+        result.textContent = "Time Out!";
+        setTimeout(function(){
+
+            gametime = 0;
+        
+            document.getElementById("card").style.display = "none";
+            document.getElementById("score").style.display = "initial";
+            document.getElementById("finalscore").textContent = "Your final Score is "+ score+ " and your playtime is "+ gametime + " seconds";
+
+        }
+            ,1000);
+        
+    }
+
+    function setquestion()
+    { 
+        
+        if(question.length > 0)
+        {
+            result.textContent = "";
+            document.getElementById("choices").innerHTML = "";
+            document.getElementById("choices").style.pointerEvents = "initial";
+
+            //choose a random question
+            current = Math.floor(Math.random() * question.length);
+            questiontext.textContent = question[current];
+            
+            //set choices
+            for(var i =0;i<4;i++)
+            {   var setchoices = document.createElement("li");
+                var newbutton = document.createElement("button");
+                newbutton.classList.add("btn-info");
+                newbutton.classList.add("rounded-pill");
+                newbutton.classList.add("m-1");
+                newbutton.textContent = options[current][i];
+                setchoices.appendChild(newbutton);
+                choices.appendChild(setchoices);
+            }
+            
+            //set answer
+            answer = solution[current];
+            
+            //Remove answer from list
+            question.splice(current,1);
+            options.splice(current,1);
+            solution.splice(current,1);
+            console.log(question);
+        
+            
+
+
+        }
+        else
+        {
+            
+            timer.textContent = "";
+            result.classList.remove();
+            result.classList.add("border-top");
+            result.classList.add("text-secondary");
+            result.textContent = "Game over";
+            if(timeplay<0){timeplay = 0;}
+            gametime = timeplay;
+            stopTime();
+            document.getElementById("card").style.display = "none";
+            document.getElementById("score").style.display = "initial";
+            document.getElementById("finalscore").textContent = "Your final Score is "+ score+ " and your playtime is "+ gametime + " seconds";
+
+        }
 
     }
 
-}
+    function inializeQuestions(){
+        options = game;
+        solution = solutionini;
+        question = questionini;
+    }
+
 
 //create event listeners
-start.addEventListener("click",startGame);
-restart.addEventListener("click",resetGame);
 
+    start.addEventListener("click",startGame);
+    restart.addEventListener("click",function(){
+        location.reload();
+    });
 
-
-choices.addEventListener("click",function(event)
+    choices.addEventListener("click",function(event)
 {
     if(event.target.tagName == "BUTTON")
     {
@@ -216,10 +215,7 @@ choices.addEventListener("click",function(event)
 
 });
 
-
-
-
-savescore.addEventListener("click",function(){
+    savescore.addEventListener("click",function(){
    
     if(player.value == "" || player.value.length > 10)
     {  
@@ -241,11 +237,12 @@ savescore.addEventListener("click",function(){
         var next = Object.keys(localStorage).length +1;
         localStorage["codequizScore" + next] = JSON.stringify(playerscore);
        
+        scorecard.setAttribute("style","display: none");
+         open("scores.html",'_self',false);
     }
    
-   scorecard.setAttribute("style","display: none");
-   open("scores.html",'_self',false);
+   
 
 });
 
-
+    
